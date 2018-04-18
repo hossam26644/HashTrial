4547fe68ed42176c756be8d71a3075c5881b26e0
Hossam Eldin Ali <35299548+hossam26644@users.noreply.github.com>
2018-04-18 06:53:16 +0200

    Add files via upload

---
+++ Hash.py
@@ -0,0 +1,14 @@
+from hashlib import sha256
+
+def mine(a, b, f):
+	for i in range (a,b):
+		s = "winner is hossam, n="+str(i)
+		h = sha256(s.encode())
+		d = h.hexdigest()
+		if d[0:f]==("0"*f):
+			print d, i
+			break
+
+
+
+mine(1,1000000000,6)
