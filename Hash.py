ae6830ecd67e85032a1db6c230889ce74d63285d
Hossam Eldin Ali <35299548+hossam26644@users.noreply.github.com>
2018-04-18 07:15:40 +0200

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,13 +1,18 @@
 from hashlib import sha256
 
-def mine(a, b, f):
+def mine(a, b, n1, n2, n3):
 	for i in range (a,b):
 		s = "winner is hossam, n="+str(i)
 		h = sha256(s.encode())
 		d = h.hexdigest()
-		if d[0:f]==("0"*f):
-			print d, i
+		if d[0:n1]==("0"*n1):
+			print d, i, n1
+		if d[0:n2]==("0"*n2):
+			print d, i, n2
+		if d[0:n3]==("0"*n3):
+			print d, i, n3
 			break
+			
 
 
 
